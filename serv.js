8eb215b059d4adaed98defb566e9e104537a9a5d
JeiChambers <jeilee88@outlook.com>
2018-11-30 19:53:53 -0600

    Played around with inline forms and added post to serv.js

--- serv.js
+++ serv.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
-var path = require('path');
+const bodyParser = require('body-parser')
+const path = require('path');
 const port = 3000;
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+let fakeDB = [];
+
+app.use(bodyParser.json());
+app.get('/home', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.post('/api/users', (req, res) => {
+	fakeDB.push(req.body);
+	console.log(fakeDB);
+	res.send("user created")
+})
+app.get('/api/users', (req, res) => {
+	res.json(fakeDB)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
