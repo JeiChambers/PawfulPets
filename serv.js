0c64ef4feca911ebe316e9e976e58bd2fa6da355
JeiChambers <jeilee88@outlook.com>
2018-12-10 19:58:32 -0600

    fixed footer floating up, added post functions and and enabled mongo db on serv.js

--- serv.js
+++ serv.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser')
 const path = require('path');
 const port = 3000;
+mongoose.connect('mongodb://localhost/Catabase')
 
-let fakeDB = [];
+.then(connection => {
+	console.log(connection)
+})
+
+const catSchema = {
+	name: {
+		type: String, 
+		required: true
+	}, 
+	age: {
+		type: Number,
+		required: true
+	}
+}
+
+const Cat = mongoose.model('Cat', catSchema)
+
+// const myCat = new Cat({name: "Tom", age: 5 })
+// myCat.save()
+// console.log(myCat)
 
 app.use(bodyParser.json());
 
@@ -12,16 +34,45 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 app.get('/css/pps.css', (req, res) => res.sendFile(path.join(__dirname + "/" + "css/pps.css")));
 app.get('/images/pupperlakec.png', (req,res) => res.sendFile(path.join(__dirname + "/" + "images/pupperlakec.png")))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/getstarted.html')));
+app.get('/getstarted', (req, res) => res.sendFile(path.join(__dirname + '/getstarted.html')));
 
 // Getting and Posting data from DB
-app.post('/api/users', (req, res) => {
-	fakeDB.push(req.body);
-	console.log(fakeDB);
-	res.send("user created")
+app.post('/api/cats', (req, res) => {
+	const maybeNewCat = new Cat(req.body)
+	maybeNewCat.save()
+		.then(result => {
+			res.json(result)
+		})
+		.catch(err => {
+			res.json({msg: err})
+		})
 })
-app.get('/api/users', (req, res) => {
-	res.json(fakeDB)
+app.get('/api/cats', (req, res) => {
+	Cat.find()
+		.then(result =>{
+			res.json(result)
+		})
+		.catch(err => {
+			res.json({msg: err})
+		})
+})
+
+app.get('/api/cats/:name', (req, res) => {
+	// Cat.findOne({name: req.params.name})
+	// .then(result =>{
+	// 	res.json(result)
+	// })
+	// .catch(err => {
+	// 	res.json({msg: err})
+	// })
+
+	Cat.findOne({name: req.params.name})
+		.then(result =>{
+			result ? res.json(result) : res.json({msg: "Sorry, no cat by that name"})			
+		})
+		.catch(err => {
+			res.json({msg: err})
+		})	
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
