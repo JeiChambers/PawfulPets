1692f53aeb3f07c432d395b0a29639284da438f5
Jei Chambers <jeilee88@outlook.com>
2019-03-03 19:05:26 -0600

    Deleted routes

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const path = require('path');
+const http = require('http');
 
+const server = http.createServer();
 const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
 
+const indexRoutes = require('./routes/index');
+const aboutRoutes = require('./routes/about');
+const startedRoutes = require('./routes/getStarted');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/index', indexRoutes);
+app.use('/about', aboutRoutes);
+app.use('/getStarted', startedRoutes);
 
 
 
